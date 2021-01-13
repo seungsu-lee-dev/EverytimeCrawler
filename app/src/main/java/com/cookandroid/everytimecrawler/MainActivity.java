7908be20d7deec3dc938759866c4b5e1a5f274d8
donghui <hhhh7611@naver.com>
2021-01-14 00:59:51 +0900

    gg

--- app/src/main/java/com/cookandroid/everytimecrawler/MainActivity.java
+++ app/src/main/java/com/cookandroid/everytimecrawler/MainActivity.java
@@ -6,15 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageButton loginbutton;
     EditText login_id, login_password;
     String sId, sPw;
-    Intent intent;
-    ImageButton loginbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,23 +21,32 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
+
+
+
         login_id = (EditText) findViewById(R.id.login_id);
         login_password = (EditText) findViewById(R.id.login_password);
 
-        intent = new Intent(this, CrawlingService.class);
-        loginbutton = (ImageButton) findViewById(R.id.loginbutton);
+        loginbutton= (ImageButton) findViewById(R.id.loginbutton);
+        loginbutton.setOnClickListener(new View.OnClickListener(){
 
-        loginbutton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                startService(intent);
-                android.util.Log.i("크롤링 인텐트로 넘어감", "startService()");
+            public void onClick(View view){
+
+                Intent intent = new Intent(getApplicationContext(), SubActivity.class);
+                startActivity(intent);
             }
         });
+
+
+
     }
 
-//    public void loginbutton(View view) {
-//        // 아이디, 비밀번호 받아온 값을 string으로
-//        sId = login_id.getText().toString();
-//        sPw = login_password.getText().toString();
-//    }
+    public void loginbutton(View view) {
+//           아이디, 비밀번호 받아온 값을 string으로
+        sId = login_id.getText().toString();
+        sPw = login_password.getText().toString();
+
+    }
+
+
 }
