77fef79a02da34d412302d7cad9503fff99b836a
chiHyoung <lyy4242@naver.com>
2021-01-14 21:52:00 +0900

    수정

---
+++ app/src/main/java/com/cookandroid/everytimecrawler/SubActivity.java
@@ -0,0 +1,99 @@
+package com.cookandroid.everytimecrawler;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import android.widget.ArrayAdapter;
+
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import java.util.ArrayList;
+
+public class SubActivity extends AppCompatActivity {
+    ImageButton setting11;
+
+    ArrayList<String> Items;
+    ArrayAdapter<String> Adapter;
+    ListView listView;
+    ImageButton btnAdd, btnDel, btnsave;
+    EditText editText;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sub_main);
+
+        Items = new ArrayList<String>();
+
+
+        Adapter = new ArrayAdapter<String>(this,
+                R.layout.simple_list_item, Items);
+        listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(Adapter);
+        listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+        editText = (EditText) findViewById(R.id.editText);
+        btnAdd = (ImageButton) findViewById(R.id.btnAdd);
+        btnDel = (ImageButton) findViewById(R.id.btnDel);
+        btnsave = (ImageButton) findViewById(R.id.savelist11);
+
+        btnAdd.setOnClickListener(listener);
+        btnDel.setOnClickListener(listener);
+        btnsave.setOnClickListener(listener);
+
+        setting11 = (ImageButton) findViewById(R.id.setting11);
+
+        setting11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btnAdd:
+                    String text = editText.getText().toString();
+                    if (text.length() != 0) {
+                        Items.add(text);
+                        editText.setText("");
+                        Adapter.notifyDataSetChanged();
+                    }
+                    break;
+                case R.id.btnDel:
+                    int pos;
+                    pos = listView.getCheckedItemPosition();
+                    if (pos != ListView.INVALID_POSITION) {
+                        Items.remove(pos);
+                        listView.clearChoices();
+                        Adapter.notifyDataSetChanged();
+                    }
+                    break;
+                case R.id.savelist11:
+
+
+            }
+
+        }
+    };
+
+
+}
+
+
+
+
+
+
