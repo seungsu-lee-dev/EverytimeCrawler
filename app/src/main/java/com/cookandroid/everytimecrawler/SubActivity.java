2f2b6ceaf99cdb7342d1df7264b58534ccac3bcb
donghui <hhhh7611@naver.com>
2021-01-18 22:33:24 +0900

    이동희 1/18

--- app/src/main/java/com/cookandroid/everytimecrawler/SubActivity.java
+++ app/src/main/java/com/cookandroid/everytimecrawler/SubActivity.java
@@ -1,63 +1,93 @@
 package com.cookandroid.everytimecrawler;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
 import android.widget.ArrayAdapter;
-
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
-
 import java.util.ArrayList;
 
 public class SubActivity extends AppCompatActivity {
-    ImageButton setting11;
 
+//--------------------------< 객체 선언 > --------------------------
+    ImageButton setting11;
     ArrayList<String> Items;
     ArrayAdapter<String> Adapter;
     ListView listView;
     ImageButton btnAdd, btnDel;
     EditText editText;
-
+    ImageButton save1,load;
+//-------------------------------------------------------------------
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sub_main);
+        setContentView(R.layout.sub_main); // sub_main의 레이아웃과 연결
 
         Items = new ArrayList<String>();
 
+        // findViewById : 해당 아이디로부터 대응되는 뷰객체를 찾는 메서드
 
+        //  listview 객체랑 activity_main의 R.id.listView랑 연결
+        listView = (ListView) findViewById(R.id.listView);
+
+        // 어텝터 생성
         Adapter = new ArrayAdapter<String>(this,
                 R.layout.simple_list_item, Items);
-        listView = (ListView) findViewById(R.id.listView);
+
+        // 어뎁터 설정 / 위에 adater설정한거를 listview에 넣는다
         listView.setAdapter(Adapter);
+
+
+        // 리스트뷰에 choicemode 속성을 설정해야 선택기능 사용가능 / 여기말고 main.xml에서도 설정가능
         listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
 
         editText = (EditText) findViewById(R.id.editText);
+
+        load = (ImageButton) findViewById(R.id.loadlist1);
+//-----------------------< add del >------------------------------------
         btnAdd = (ImageButton) findViewById(R.id.btnAdd);
         btnDel = (ImageButton) findViewById(R.id.btnDel);
 
         btnAdd.setOnClickListener(listener);
         btnDel.setOnClickListener(listener);
 
+//-------------------------- setting 버튼 부분 -----------------------------------
+
         setting11 = (ImageButton) findViewById(R.id.setting11);
 
         setting11.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
+                Intent intent11 = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent11);
             }
         });
+
+        save1 = (ImageButton) findViewById(R.id.savelist11);
+
+
+
     }
 
+
+//-----------------------------------------------------------------------------------
+//    public void onClick(View view){
+//        switch (view.getId()){
+//            case R.id.savelist11:
+//                startActivity(new Intent(this, Dialog.class));
+//                break;
+ //       }
+    //}
+//---------------------------<  add , del 버튼 >-------------------------------------
     private View.OnClickListener listener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -67,7 +97,7 @@ public class SubActivity extends AppCompatActivity {
                     if (text.length() != 0) {
                         Items.add(text);
                         editText.setText("");
-                        Adapter.notifyDataSetChanged();
+                        Adapter.notifyDataSetChanged(); // listview 갱신할때 사용하는 메소드
                     }
                     break;
                 case R.id.btnDel:
@@ -83,6 +113,24 @@ public class SubActivity extends AppCompatActivity {
 
         }
     };
+
+//-------------------------------- 팝업창 ------------------------------
+    public void OnClickHandler(View view)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("").setMessage("목록이 추가되었습니다");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int id)
+            {
+                Toast.makeText(getApplicationContext(), "OK Click", Toast.LENGTH_SHORT).show();
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+
+        alertDialog.show();
+    }
 }
 
 
