4a04fde841b958bab8929e09bf87fc3a434b8af1
seungsu-lee <seungsu-lee-dev@gmail.com>
2021-01-25 00:05:56 +0900

    승수 20.01.25

--- app/src/main/java/com/cookandroid/everytimecrawler/MainActivity.java
+++ app/src/main/java/com/cookandroid/everytimecrawler/MainActivity.java
@@ -12,8 +12,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-//import org.jsoup.nodes.Element;
-//import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.HashMap;
