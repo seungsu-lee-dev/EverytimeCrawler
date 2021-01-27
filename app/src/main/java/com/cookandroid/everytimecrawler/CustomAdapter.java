c34e5728a9712f6a143b19be62c3d07f976b710c
donghui <hhhh7611@naver.com>
2021-01-27 21:09:38 +0900

    시범용

---
+++ app/src/main/java/com/cookandroid/everytimecrawler/CustomAdapter.java
@@ -0,0 +1,15 @@
+package com.cookandroid.everytimecrawler;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.List;
+
+
+public class CustomAdapter {
+}
